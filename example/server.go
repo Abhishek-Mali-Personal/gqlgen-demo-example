62efb98cd1c4cfb36028e95156b4487e3006fa6a
Abhishek-Mali-Simform <abhishek.m@simformsolutions.com>
2023-12-07 10:15:49 +0530

    GraphQL: Added Docker Updated .env Variables and added Dockerfile and docker-compose.yml file with configuration for local database connection.

--- example/server.go
+++ example/server.go
@@ -60,7 +60,14 @@ func main() {
 	dbDown := flag.Bool("db_down", false,
 		"Specifies if database needs to drop while starting application.")
 	flag.Parse()
-	DB, err := postgres.New(os.Getenv("DB_DSN"))
+	dbPort := os.Getenv("DB_PORT")
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		dbHost, dbUser, dbPassword, dbName, dbPort)
+	DB, err := postgres.New(dsn)
 	if err != nil {
 		panic(err)
 	}
